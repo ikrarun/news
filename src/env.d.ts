1991f89d95bd083dae30d1ecb695c35b90903886
ikrarun <wowdeveloper@protonmail.com>
2023-09-23 00:47:31 +0530

    Working News Box

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,28 @@
 /// <reference types="astro/client" />
+
+
+// Define an interface for the "source" object
+interface NewsSource {
+    id: string | null;
+    name: string;
+  }
+  
+  // Define an interface for each article
+  interface NewsArticle {
+    source: NewsSource;
+    author: string | null;
+    title: string;
+    description: string | null;
+    url: string;
+    urlToImage: string | null;
+    publishedAt: string;
+    content: string | null;
+  }
+  
+  // Define the main interface for the JSON data
+  interface NewsData {
+    status: string;
+    totalResults: number;
+    articles: NewsArticle[];
+  }
+  
